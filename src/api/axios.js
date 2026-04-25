3ab6c21e093a76e3763aafc90498f2466de991a3
Muhammad Ahsan Noor <ranaahsanrajpoot08@gmail.com>
2026-04-25 16:22:15 +0500

    Fix axios baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,27 +5,10 @@ const api = axios.create({
   headers: { 'Accept': 'application/json' },
 });
 
-axios.get(`${import.meta.env.VITE_API_URL}/products`)
-  .then(res => console.log(res.data))
-
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
-
-// axios.post(`${import.meta.env.VITE_API_URL}/cart`, {
-//   product_id: product.id,
-//   quantity: 1
-// }, {
-//   headers: {
-//     Authorization: `Bearer ${localStorage.getItem("token")}`
-//   }
-// })
-// .then(res => console.log(res.data))
-// .catch(err => {
-//   console.log(err.response); // 👈 yahan actual error milega
-// });
 
 export default api;
